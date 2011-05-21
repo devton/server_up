668b011e5e81374175fb6ab173e15de77a6b4378
Antonio Roberto <antonio@webmais.net.br>
2011-05-21 14:43:45 -0300

    use exec with variables

--- server_up.sh
+++ server_up.sh
@@ -3,25 +3,24 @@ echo "=== Basic config"
 
 echo "=== Installing basic server programs... (mysql, htop and more... :] )"
 install_command="apt-get install -y mysql-server libmysqld-dev libmysql++3 libmysql++-dev htop build-essential autoconf libcurl4-openssl-dev"
-echo `$install_command`
+exec $install_command
 
 echo "=== Creating /opt/fonts for save the fonts"
 mkdir_fonts="mkdir /opt/fonts"
-echo `$mkdir_fonts`
+exec $mkdir_fonts
 
 echo "=== Ruby 1.9.2 download..."
 wget_ruby="wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz;tar -zxvf ruby-1.9.2-p180.tar.gz;cd ruby-1.9.2-p180"
-echo `$wget_ruby`
+exec $wget_ruby
 
 echo "=== Configure and Make ;) ..."
 make_ruby="./configure; make; make install"
-echo `$make_ruby`
-echo "=== Finished the ruby "`ruby -v`
+exec $make_ruby
 
 echo "Other libs..."
 make_other_libs="cd ext/zlib;ruby extconf.rb;make;make install"
-echo `$make_other_libs`
+exec $make_other_libs
 
 echo "=== Download passenger"
 wget_passenger_and_install="wget http://rubyforge.org/frs/download.php/74471/passenger-3.0.5.tar.gz;tar -zxvf passenger-3.0.5.tar.gz; cd passenger-3.0.5; ./bin/passenger-install-nginx-module"
-echo `$wget_passenger_and_install`
+exec $wget_passenger_and_install
