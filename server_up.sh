59dd4835b1684f7892101b9d286a8b50f1b978fb
Antonio Roberto <antonio@webmais.net.br>
2011-05-21 15:06:05 -0300

    cd into fonts

--- server_up.sh
+++ server_up.sh
@@ -2,9 +2,9 @@
 echo "=== Basic config"
 echo `apt-get install -y mysql-server libmysqld-dev libmysql++3 libmysql++-dev htop build-essential autoconf libcurl4-openssl-dev`
 echo `mkdir /opt/fonts; cd /opt/fonts`
-echo `wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz`
-echo `tar -zxvf ruby-1.9.2-p180.tar.gz; cd ruby-1.9.2-p180`
+echo `cd /opt/fonts; wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz`
+echo `cd /opt/fonts; tar -zxvf ruby-1.9.2-p180.tar.gz; cd ruby-1.9.2-p180`
 echo `./configure; make; make install`
 echo `cd ext/zlib; ruby extconf.rb; make; make install`
-echo `wget http://rubyforge.org/frs/download.php/74471/passenger-3.0.5.tar.gz`
-echo `tar -zxvf passenger-3.0.5.tar.gz; cd passenger-3.0.5; ./bin/passenger-install-nginx-module`
+echo `cd /opt/fonts; wget http://rubyforge.org/frs/download.php/74471/passenger-3.0.5.tar.gz`
+echo `cd /opt/fonts; tar -zxvf passenger-3.0.5.tar.gz; cd passenger-3.0.5; ./bin/passenger-install-nginx-module`
