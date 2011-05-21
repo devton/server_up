0b3f1dfdb49d9033de33a1ee07af34c239be9727
Antonio Roberto <antonio@webmais.net.br>
2011-05-21 14:56:32 -0300

    cd into /opt/fonts

--- server_up.sh
+++ server_up.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "=== Basic config"
 install_command="apt-get install -y mysql-server libmysqld-dev libmysql++3 libmysql++-dev htop build-essential autoconf libcurl4-openssl-dev"
-mkdir_fonts="mkdir /opt/fonts"
-wget_ruby="wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz /opt/"
-enter_ruby_dir="tar -zxvf ruby-1.9.2-p180.tar.gz;cd ruby-1.9.2-p180 /opt/"
+mkdir_fonts="mkdir /opt/fonts; cd /opt/fonts"
+wget_ruby="wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz"
+enter_ruby_dir="tar -zxvf ruby-1.9.2-p180.tar.gz;cd ruby-1.9.2-p180"
 make_ruby="./configure; make; make install"
 make_other_libs="cd ext/zlib;ruby extconf.rb;make;make install"
 wget_passenger="wget http://rubyforge.org/frs/download.php/74471/passenger-3.0.5.tar.gz"
