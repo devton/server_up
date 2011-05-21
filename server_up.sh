db3a71f4d969b1d05aa91fe03d71fc876ba98239
Antonio Roberto <antonio@webmais.net.br>
2011-05-21 15:54:38 -0300

    only ruby

--- server_up.sh
+++ server_up.sh
@@ -4,7 +4,8 @@ echo `apt-get install -y mysql-server libmysqld-dev libmysql++3 libmysql++-dev h
 echo `mkdir /opt/fonts`
 echo `cd /opt/fonts; wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz`
 echo `cd /opt/fonts; tar -zxvf ruby-1.9.2-p180.tar.gz`
-echo `cd /opt/fonts/ruby-1.9.2-p180/; ./configure; /opt/fonts/ruby-1.9.2-p180/make; /opt/fonts/ruby-1.9.2-p180/make install`
-echo `ruby /opt/fonts/ruby-1.9.2-p180/ext/zlib/extconf.rb; /opt/fonts/ruby-1.9.2-p180/ext/zlib/make; /opt/fonts/ruby-1.9.2-p180/ext/zlib/make install`
+echo `cd /opt/fonts/ruby-1.9.2-p180/; ./configure; make; make install`
+echo `cd /opt/fonts/ruby-1.9.2-p180/ext/zlib; ruby extconf.rb; make; make install`
 echo `cd /opt/fonts; wget http://rubyforge.org/frs/download.php/74471/passenger-3.0.5.tar.gz`
-echo `cd /opt/fonts; tar -zxvf passenger-3.0.5.tar.gz; ./opt/fonts/passenger-3.0.5/bin/passenger-install-nginx-module`
+echo `gem install rack`
+
